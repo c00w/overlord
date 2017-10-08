86d481062135e7bb65dd346ed63f37d9594401af
Colin L. Rice <colin@daedrum.net>
2017-10-07 21:05:45 -0400

    Make it serve hello world

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func h(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+	log.Print(r)
+}
+
+func run(arg ...string) {
+
+	out, err := exec.Command(arg[0], arg[1:]...).CombinedOutput()
+	log.Printf("Ran %v, err = %v", arg, err)
+	log.Printf("Output ----\n %s", string(out))
+}
+
+func main() {
+
+	run("modprobe", "v4l2_common")
+	run("go", "env")
+	http.HandleFunc("/", h)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
