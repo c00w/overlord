9be005f73d6d6525348cba375e70b2ed7baf3c38
Colin L. Rice <colin@daedrum.net>
2017-10-08 00:01:34 -0400

    Set up basic recording

--- main.go
+++ main.go
@@ -18,12 +18,26 @@ func run(arg ...string) {
 	log.Printf("Output ----\n %s", string(out))
 }
 
+func record() {
+	run("ffmpeg",
+		"-f", "v4l2",
+		"-input_format", "h264",
+		"-framerate", "30",
+		"-video_size", "1920x1080",
+		"-t", "61",
+		"-i", "/dev/video0",
+		"-c", "copy",
+		"-f", "segment",
+		"-segment_time", "60",
+		"-strftime", "1", "/data/%s.mkv")
+}
+
 func main() {
 
 	log.Printf("starting")
 	run("modprobe", "v4l2_common")
-	run("cat", "/proc/cpuinfo")
-	run("ffmpeg")
+	run("df -h")
+	go record()
 	http.HandleFunc("/", h)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
